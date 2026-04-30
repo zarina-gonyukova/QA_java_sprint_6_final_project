618706b0b6baba879652f81aa0804996dc8a6aed
Zarina Gonyukova <zarina.gonyukova@yandex.ru>
2026-04-30 14:55:48 +0500

    fix: LionMockTest - one check per test, mock returns real data

--- src/test/java/com/example/LionMockTest.java
+++ src/test/java/com/example/LionMockTest.java
@@ -10,19 +10,29 @@ import static org.mockito.Mockito.*;
 public class LionMockTest {
 
     @Test
-    public void getFoodUsesPredatorEatMeat() throws Exception {
+    public void getFoodReturnsPredatorFood() throws Exception {
         Predator predatorMock = mock(Predator.class);
-        when(predatorMock.eatMeat()).thenReturn(List.of("Животные"));
+        when(predatorMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
 
         Lion lion = new Lion(predatorMock, "Самец");
         List<String> food = lion.getFood();
 
-        assertEquals(List.of("Животные"), food);
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), food);
+    }
+
+    @Test
+    public void getFoodCallsPredatorEatMeatOnce() throws Exception {
+        Predator predatorMock = mock(Predator.class);
+        when(predatorMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Lion lion = new Lion(predatorMock, "Самец");
+        lion.getFood();
+
         verify(predatorMock, times(1)).eatMeat();
     }
 
     @Test
-    public void getKittensDelegatesToFelineWhenPredatorIsFeline() throws Exception {
+    public void getKittensReturnsFelineValue() throws Exception {
         Feline felineMock = mock(Feline.class);
         when(felineMock.getKittens()).thenReturn(3);
 
@@ -30,6 +40,16 @@ public class LionMockTest {
         int kittens = lion.getKittens();
 
         assertEquals(3, kittens);
+    }
+
+    @Test
+    public void getKittensCallsFelineGetKittensOnce() throws Exception {
+        Feline felineMock = mock(Feline.class);
+        when(felineMock.getKittens()).thenReturn(3);
+
+        Lion lion = new Lion(felineMock, "Самка");
+        lion.getKittens();
+
         verify(felineMock, times(1)).getKittens();
     }
 }
